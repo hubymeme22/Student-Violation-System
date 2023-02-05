8a46d0564c981b9443c0d79cd90beb15f6f1adf8
Sanjero <gelosantos72@gmail.com>
2023-02-05 21:46:41 +0800

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+// Pages
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <div>
-      <h1>Student Violation Records</h1>
+      <Routes>
+        {/* Login Route */}
+        <Route path="/" element={<Login />} />
+
+        {/* Invalid Route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NotFound() {
+  return <div>Page does not exist</div>;
+}
+
+export default NotFound;
