d1130ad2666448649eb5ac5c971712ad685d93ec
Sanjero <gelosantos72@gmail.com>
2023-02-05 22:19:15 +0800

    add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { GlobalStyles } from './theme/globalStyles';
 
 // Pages
 import Login from './pages/Login';
@@ -8,6 +9,7 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <div>
+      <GlobalStyles />
       <Routes>
         {/* Login Route */}
         <Route path="/" element={<Login />} />
