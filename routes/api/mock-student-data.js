28f7ea380fdca5e34c2ec1ae2b9c5f88e1b9bfc3
HueHueberry <huby.new1112@gmail.com>
2023-02-12 20:55:54 +0800

    updated mock server for retrieving all and specific student route

--- routes/api/mock-student-data.js
+++ routes/api/mock-student-data.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const student = Router();
 student.get('/', (req, res) => {
-    res.json({
+    res.json([{
         "username": "karl_99",
         "email": "karl@email.com",
         "details": {
@@ -26,7 +26,39 @@ student.get('/', (req, res) => {
             { "category": "Attendance", "specific": "Late" },
             { "category": "Attendance", "specific": "Late" }
         ]
-    });
+    }]);
+});
+
+student.get('/:studentUsername', (req, res) => {
+    if (req.params.studentUsername == 'karl_99') {
+        res.json({
+            "username": "karl_99",
+            "email": "karl@email.com",
+            "details": {
+                "contact": "+639123456789",
+                "birthday": '09-02-2023',
+                "sex": "Male",
+                "fullname": {
+                    "first": "Karl",
+                    "mid": "M",
+                    "last": "Aguilar"
+                },
+                "familycontact": {
+                    "father": "Father Karl Aguilar",
+                    "mother": "Mother Karl Aguilar",
+                    "father-contact": "+639234565431",
+                    "mother-contact": "+639234565432"
+                }
+            },
+            "violations": [
+                { "category": "Attendance", "specific": "Cutting Classes" },
+                { "category": "Attendance", "specific": "Late" },
+                { "category": "Attendance", "specific": "Late" }
+            ]
+        });
+    } else {
+        res.json({});
+    }
 });
 
 export default student;
