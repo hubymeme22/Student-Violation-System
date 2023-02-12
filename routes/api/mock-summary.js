4561e37cca3abdd84f3a6e36dcda7ad90a8284f0
HueHueberry <huby.new1112@gmail.com>
2023-02-12 21:06:49 +0800

    Changed format for monthSummary data

--- routes/api/mock-summary.js
+++ routes/api/mock-summary.js
@@ -5,7 +5,20 @@ summary.get('/', (req, res) => {
     res.json({
         violationCount: 32,
         studentsCount: 1250,
-        monthSummary: [12, 11, 2, 0, 3, 23, 1, 3, 0, 0, 11, 12]
+        monthSummary: [
+            {'month': 'January', 'violations': 32},
+            {'month': 'February', 'violations': 12},
+            {'month': 'March', 'violations': 43},
+            {'month': 'April', 'violations': 13},
+            {'month': 'May', 'violations': 2},
+            {'month': 'June', 'violations': 2},
+            {'month': 'July', 'violations': 1},
+            {'month': 'August', 'violations': 56},
+            {'month': 'September', 'violations': 12},
+            {'month': 'October', 'violations': 13},
+            {'month': 'November', 'violations': 2},
+            {'month': 'December', 'violations': 3}
+        ]
     });
 });
 
