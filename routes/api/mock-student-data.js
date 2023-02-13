87a5464f7b494bf0a67ef3de2afb81cbba08c9d1
HueHueberry <huby.new1112@gmail.com>
2023-02-14 01:51:06 +0800

    Shortened data for retrieving data on /student route

--- routes/api/mock-student-data.js
+++ routes/api/mock-student-data.js
@@ -6,26 +6,12 @@ student.get('/', (req, res) => {
         "username": "karl_99",
         "email": "karl@email.com",
         "details": {
-            "contact": "+639123456789",
-            "birthday": '09-02-2023',
-            "sex": "Male",
             "fullname": {
                 "first": "Karl",
                 "mid": "M",
                 "last": "Aguilar"
-            },
-            "familycontact": {
-                "father": "Father Karl Aguilar",
-                "mother": "Mother Karl Aguilar",
-                "father-contact": "+639234565431",
-                "mother-contact": "+639234565432"
             }
-        },
-        "violations": [
-            { "category": "Attendance", "specific": "Cutting Classes" },
-            { "category": "Attendance", "specific": "Late" },
-            { "category": "Attendance", "specific": "Late" }
-        ]
+        }
     }]);
 });
 
